f5ad01e7fa0c083b8817d3d595a5b9374ecdcbc6
Patrick McKnight <patrickgmcknight@gmail.com>
2023-03-19 21:40:40 -0700

    started index.js class sequelize associations

--- models/index.js
+++ models/index.js
@@ -5,8 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'primary_cat'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_cat'
+});
 
 // Products belongToMany Tags (through ProductTag)
 
