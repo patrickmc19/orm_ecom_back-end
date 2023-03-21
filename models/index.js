2fc8c39c22ebcb01ec4987b87a96291a13bb3936
Patrick McKnight <patrickgmcknight@gmail.com>
2023-03-21 14:06:42 -0700

    completed model associations and started category routes

--- models/index.js
+++ models/index.js
@@ -23,8 +23,22 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'prod_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'tags_prod'
+})
 
 module.exports = {
   Product,
